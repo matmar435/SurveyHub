eda111964ab76db3b2444454a4c28fd8b2c78eba
matmar435 <matmar435@gmail.com>
2026-01-17 23:44:34 +0100

    corect ConnetcionStrings

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<SurveyDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
